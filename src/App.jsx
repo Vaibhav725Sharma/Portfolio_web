55c8cd3e7933bb4b94c976a14d3652d9121f1a7f
Vaibhav725Sharma <Vsharma4038@gmail.com>
2024-07-29 12:31:56 +0530

    solving an issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
