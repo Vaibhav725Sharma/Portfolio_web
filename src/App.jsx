d583064ab13137b0bdddc77bc171d0cdd86abfc1
Vaibhav725Sharma <Vsharma4038@gmail.com>
2024-07-29 12:37:38 +0530

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/parallax";
+import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
